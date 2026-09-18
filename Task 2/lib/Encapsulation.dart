4058664d4f53b650f3482916d26a0913cfe6463b
15RAS16 <raj15052008sharma@gmail.com>
2026-09-18 19:35:55 +0530

    Encaplculation
    Adding encapsulation example.

---
+++ Task 2/lib/Encapsulation.dart
@@ -0,0 +1,17 @@
+class Pokemon {
+  int _level;
+  String _name;
+  String type;
+  Pokemon({required this._level , required this._name , required this.type});
+
+  void display(){
+    print("Pokemon Name : $_name");
+    print("Pokemon Type : $type");
+    print("Pokemon Level : $_level");
+
+  }
+}
+void main(){
+  Pokemon entry1 = Pokemon(level: 50, name: "Pikachu", type: "Electric");
+  entry1.display();
+}
